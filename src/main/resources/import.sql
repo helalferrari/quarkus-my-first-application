b1bbe0a53891f072a277e09d3447811ad26f8648
helalferrari <ferrari.work@gmail.com>
2026-01-16 08:19:02 -0300

    Aula sobre persistencia quarkus + panache + h2

---
+++ src/main/resources/import.sql
@@ -0,0 +1,8 @@
+INSERT INTO Pessoa(id, nome, anonascimento) VALUES (1, 'Ferrari', 1983);
+INSERT INTO Pessoa(id, nome, anonascimento) VALUES (2, 'Cabral', 1984);
+INSERT INTO Pessoa(id, nome, anonascimento) VALUES (3, 'Arthur', 2008);
+INSERT INTO Pessoa(id, nome, anonascimento) VALUES (4, 'Melina', 1982);
+INSERT INTO Pessoa(id, nome, anonascimento) VALUES (5, 'Hagadir', 1960);
+INSERT INTO Pessoa(id, nome, anonascimento) VALUES (6, 'Henrique', 2018);
+
+ALTER SEQUENCE pessoa_seq RESTART WITH 7;
